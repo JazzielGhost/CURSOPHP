bddc130c37d34ca33f29d3f1ff3f081c48f80244
JazzielGod <jazzielbh@gmail.com>
2023-10-11 14:10:39 -0700

    update - poo ll

--- ejerciciosFacil/poo_I.php
+++ ejerciciosFacil/poo_I.php
@@ -7,14 +7,37 @@
 </head>
 <body>
     <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
     class Coche{
-
+        public  int $ruedas;
+        public string $color;
+        public int $motor;
+        public function __construct(){//metodo constructor
+            $this -> ruedas = 4;
+            $this -> color = "";
+            $this -> motor = 1600;
+        }
+        function arrancar(){
+            echo "El coche arranca <br>";
+        }
+        function girar(){
+            echo "El coche gira <br>";
+        }
+        function frenar(){
+            echo "El coche frena <br>";
+        }
     }
     
-    $ferrari = new Coche();
+    $ferrari = new Coche(); //instancia
     $mazda = new Coche();
     $seat = new Coche();
     
+    $ferrari -> girar();
+    
+    echo $ferrari -> ruedas;
 
     ?>
 </body>
