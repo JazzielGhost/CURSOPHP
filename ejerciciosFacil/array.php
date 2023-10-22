381bd4fc6a38e8072db79d9be3098ae96b49a02b
JazzielGod <jazzielbh@gmail.com>
2023-10-21 20:52:07 -0700

    update - arrays ll

--- ejerciciosFacil/array.php
+++ ejerciciosFacil/array.php
@@ -7,15 +7,49 @@
 </head>
 <body>
     <?php
-        /*
+/*
             $semana[]="Lunes";
             $semana[]="Martes";
             $semana[]="Miercoles";
-        */
-        /*array asociativo*/
+       
+        array asociativo
         $datos=array("Nombre"=>"Juan","Apellido"=>"Perez","Edad"=>25);
-        
+
+        $datos="Hola";
         echo $datos["Nombre"];
+        if (is_array($datos)) {
+            echo "Es un array";
+        }else{
+            echo "No es un array";
+        }
+
+    $datos=array("Nombre"=>"Juan","Apellido"=>"Perez","Edad"=>25);
+    foreach($datos as $clave=>$valor){
+        echo "A $clave le corresponde $valor <br>";
+    }
+
+    $semana[]="Lunes";
+    $semana[]="Martes";
+    $semana[]="Miercoles";
+    for($i=0;$i<count($semana);$i++){
+        echo $semana[$i]."<br>";
+    }
+
+    $datos=array("Nombre"=>"Juan","Apellido"=>"Perez","Edad"=>25);
+    $datos["Sexo"]="Masculino";
+    
+    foreach($datos as $clave=>$valor){
+        echo "A $clave le corresponde $valor <br>";
+    }
+    
+*/
+    
+    $semana=array("Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo");
+    //orden alfabeticamente
+    sort($semana);
+    for($i=0;$i<count($semana);$i++){
+        echo $semana[$i]."<br>";
+    }
 
     ?>
 </body>
