d6a662550d1502b2c9d965b9af4ba22c372971fd
JazzielGod <jazzielbh@gmail.com>
2023-10-27 20:42:38 -0700

    update - obteniendo registros

--- ejerciciosFacil/conexion_bd.php
+++ ejerciciosFacil/conexion_bd.php
@@ -12,25 +12,34 @@
     $db_pass="jaziel1218*[=}*";
     $db_name="CURSO_PHP_MYSQL";
 
-    $conexion = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    $conexion = mysqli_connect($db_host, $db_user, $db_pass);
 
+    if(mysqli_connect_errno()){
+        echo "Fallo al conectar con la base de datos";
+        exit(); 
+    }
+
+    mysqli_select_db($conexion, $db_name) or die ("No se encuentra la base de datos");
+
+    //mysqli_set_charset($conexion, "utf8");
 
     $consulta = "SELECT * FROM USERS";
     $resultados = mysqli_query($conexion, $consulta);
+
+    while($fila = mysqli_fetch_row($resultados)){
+
+        echo $fila[0] . " ";
+        
+        echo $fila[1] . " ";
     
-    $fila = mysqli_fetch_row($resultados);
-
-    echo $fila[0] . " ";
+        echo $fila[2] . " ";
     
-    echo $fila[1] . " ";
+        echo $fila[3] . " ";
+        
+        echo "<br>";
+    }
 
-    echo $fila[2] . " ";
-
-    echo $fila[3] . " ";
-    
-    
-
-
+    mysqli_close($conexion);
 
     ?>
 </body>
