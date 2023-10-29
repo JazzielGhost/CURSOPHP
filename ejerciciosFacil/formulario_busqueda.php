a6eab305371933c966356b06451de81179d1b10f
JazzielGod <jazzielbh@gmail.com>
2023-10-28 23:39:26 -0700

    update - busquedas ll

---
+++ ejerciciosFacil/formulario_busqueda.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="pagina_busqueda.php" method="get" >
+        <label for="">Buscar: <input type="text" name="buscar"> </label>
+        <input type="submit" value="Enviar">
+    </form>
+
+    <?php
+
+    
+    ?>
+</body>
+</html>
---
+++ ejerciciosFacil/pagina_busqueda.php
@@ -0,0 +1,26 @@
+<?php
+
+$busqueda = $_GET['buscar'];
+
+require 'datos_conexion.php';
+
+$consulta = "SELECT * FROM ARTICULOS where SECCIàN like '%$busqueda%'";
+
+$resultados = mysqli_query($conexion, $consulta);
+while ($fila = mysqli_fetch_array($resultados, MYSQLI_ASSOC)) {
+    echo "<table> <tr> <td>";
+    echo "<td>" . $fila['SECCIàN'] . "  </td>";
+
+    echo "<td>" . $fila['FECHA'] . "  </td>";
+
+    echo "<td>" . $fila['NOMBRE ARTÖCULO'] . "  </td>";
+
+    echo "<td>" . $fila['PRECIO'] . "  </td>";
+
+    echo "<td>" . $fila['PAÖS DE ORIGEN'] . "  </td>  </tr> </table>";
+
+    echo "<br>";
+    echo "<br>";
+}
+
+?>
