419072a02be181e6c05685f1fd9c03f77a09cb95
JazzielGod <jazzielbh@gmail.com>
2024-03-24 12:11:32 -0700

    update : busqueda mejorada

--- medio/pagina_busqueda.php
+++ medio/pagina_busqueda.php
@@ -5,12 +5,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>pdo</title>
 </head>
+
+    <style>
+        table{
+            width:300px;
+            margin:auto;
+            background-color:#FFC;
+            border:2px solid #F00;
+            padding:5px;
+        }
+        td{
+            text-align:center;
+        }
+    </style>
+
 <body>
     
     <form action="pagina_busqueda_pdo.php" method="get">
-        <label for="buscar">Buscar:</label>
-        <input type="text" name="buscar">
-        <input type="submit" name="enviando" value="Dale!">
+    <table>
+        <tr>
+            <td>
+               Seccion:
+            </td>
+            <td>
+               <input type="text" name="seccion">
+            </td>
+        </tr>
+        <tr>
+            <td>
+               Pais de origen:
+            </td>
+            <td>
+               <input type="text" name="p_origen">
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+               <input type="submit" name="enviando" value="Dale!">
+            </td>
+        </tr>
+    </table>
     </form>
 
 </body>
--- medio/pagina_busqueda_pdo.php
+++ medio/pagina_busqueda_pdo.php
@@ -1,6 +1,7 @@
 <?php
 
-    $busqueda = $_GET['buscar'];
+    $busqueda_sec = $_GET['seccion'];
+    $busqueda_porig = $_GET['p_origen'];
 
     try {
         $base = new PDO('mysql:host=localhost;dbname=CURSO_PHP_MYSQL', 'root', 'jaziel1218*[=}*');
@@ -8,11 +9,11 @@
         
         $base->exec("SET CHARACTER SET utf8");
 
-        $sql = "SELECT SECCIàN, `NOMBRE ARTÖCULO`, FECHA, `PAÖS DE ORIGEN` FROM ARTICULOS WHERE `SECCIàN` = ?";
+        $sql = "SELECT SECCIàN, `NOMBRE ARTÖCULO`, FECHA, `PAÖS DE ORIGEN` FROM ARTICULOS WHERE `SECCIàN` = :n_seccion AND `PAÖS DE ORIGEN` = :n_porig";
 
         $resultado = $base->prepare($sql);
 
-        $resultado -> execute(array($busqueda));
+        $resultado -> execute(array(":n_seccion" => $busqueda_sec, ":n_porig" => $busqueda_porig));
 
         while($registro = $resultado->fetch(PDO::FETCH_ASSOC)) {
             echo "Sección: " . $registro['SECCIàN'] . " Nombre Artículo: " . $registro['NOMBRE ARTÖCULO'] . " Fecha: " . $registro['FECHA'] . " País de Origen: " . $registro['PAÖS DE ORIGEN'] . "<br>";
