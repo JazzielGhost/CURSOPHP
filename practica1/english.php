b87253a54c27c2a840fb1e676da62c82620643a2
JazzielGod <jazzielbh@gmail.com>
2024-04-15 09:58:43 -0700

    add: Cookies III Práctica 1.

---
+++ practica1/english.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>English</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            display: grid;
+            place-items: center;
+        }
+        img {
+            margin: 10px;
+            padding: 10px;
+        }
+        p {
+            text-align: justify;
+            margin: 10px;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hello!! You're Welcome</h1>
+
+    <p>This is English language</p>
+
+    <img src="img/ing.png" alt="">
+    
+    <p>The history of Mexico is a fascinating and multifaceted chronicle that spans miles of years of civilization. Before the arrival of Europeans, the Mexican territory was populated by diverse indigenous cultures that flourished in agriculture, astronomy, architecture and art. Among them, the Olmecs, Mayans, Zapotecs and Aztecs stand out, who left an impressive cultural and architectural legacy.</p>
+
+</body>
+</html>
---
+++ practica1/spanish.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Espanish</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            display: grid;
+            place-items: center;
+        }
+        img {
+            margin: 10px;
+            padding: 10px;
+        }
+        p {
+            text-align: justify;
+            margin: 10px;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hola!! Bienvenido</h1>
+
+    <p>Este es el idioma español</p>
+
+    <img src="img/mex.png" alt="">
+    
+    <p>La historia de México es una crónica fascinante y multifacética que abarca miles de años de civilización. Antes de la llegada de los europeos, el territorio mexicano estaba poblado por diversas culturas indígenas que florecieron en la agricultura, la astronomía, la arquitectura y el arte. Entre ellas destacan los Olmecas, Mayas, Zapotecas y Aztecas, quienes dejaron un legado cultural y arquitectónico impresionante.</p>
+
+</body>
+</html>
